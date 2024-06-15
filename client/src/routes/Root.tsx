7aac145e38f51ffdc5f56ea15e00cb19f3d58d96
Ethan Bonsignori <ebonsignori@gmail.com>
2024-06-15 09:32:46 -0700

    add typed display WIP

--- client/src/routes/Root.tsx
+++ client/src/routes/Root.tsx
@@ -1,46 +1,55 @@
-import { Box, LinearProgress } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import { useQuery } from "@tanstack/react-query";
-import { FC } from "react";
-import { getFeatureRequests } from "../api/api";
-import FeatureRequest from "../components/FeatureRequest";
+import { FC, useEffect, useRef } from "react";
+import Typed from "typed.js";
 import Layout from "../components/Layout";
 
 const Root: FC = () => {
-  const {
-    isLoading,
-    error,
-    data: featureRequests,
-  } = useQuery({
-    queryKey: ["feature-requests"],
-    queryFn: getFeatureRequests,
-  });
+  const typedRef = useRef(null);
 
-  if (isLoading)
-    return (
-      <Layout>
-        <Box sx={{ width: "100%" }}>
-          <LinearProgress />
-        </Box>
-      </Layout>
-    );
+  useEffect(() => {
+    const typed = new Typed(typedRef.current, {
+      strings: [
+        "users.",
+        "audience.",
+        "viewers.",
+        "customers.",
+        "fans.",
+        "subscribers.",
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true,
+    });
 
-  if (error) return <Layout>Error: {error.message}</Layout>;
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+  // const {
+  //   isLoading,
+  //   error,
+  //   data: featureRequests,
+  // } = useQuery({
+  //   queryKey: ["feature-requests"],
+  //   queryFn: getFeatureRequests,
+  // });
 
-  if (!featureRequests)
-    return <Layout>No data yet, try creating a feature request</Layout>;
-
+  // FIXME: landing page not as early return
   return (
     <Layout>
       <Grid container spacing={1}>
-        {featureRequests.map((featureRequest) => (
-          <Grid item xs={12}>
-            <FeatureRequest
-              key={featureRequest.id}
-              featureRequest={featureRequest}
-            />
-          </Grid>
-        ))}
+        <Grid item xs={12} className="">
+          <Typography variant="h2">Collect feedback.</Typography>
+          <Typography variant="h2">
+            Engage your <span className="" ref={typedRef} />
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary">
+            See more
+          </Button>
+        </Grid>
       </Grid>
     </Layout>
   );
